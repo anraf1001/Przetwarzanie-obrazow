86d05cbfb05b2998a09fff813557f36dddd35b0f
Andrzej Rafalski <anraf1001@gmail.com>
2021-10-24 10:06:31 +0200

    Lab3 - builtin blur

--- Lab3/scanning.py
+++ Lab3/scanning.py
@@ -2,18 +2,24 @@ import cv2
 import numpy as np
 
 def blur(img):
+    img_new = img.copy()
     for row in range(1, len(img) - 1):
         for col in range(1, len(img[row]) - 1):
-            img[row, col] = np.mean(img[row-1:row+2, col-1:col+2])
+            img_new[row, col] = np.mean(img[row-1:row+2, col-1:col+2])
 
-    return img
+    return img_new
+
+def builtin_blur(img):
+    return cv2.blur(img, (3, 3))
 
 img = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
 img.reshape(img.shape[0] * img.shape[1])[::3] = 255
 
 img_blurred = blur(img)
+img_builtin = builtin_blur(img)
 
 cv2.namedWindow('image')
+cv2.namedWindow('image built in blur')
 
 while True:
     # sleep for 10 ms waiting for user to press some key, return -1 on timeout
@@ -23,6 +29,7 @@ while True:
         break
 
     cv2.imshow('image', img_blurred)
+    cv2.imshow('image built in blur', img_builtin)
 
 # closes all windows (usually optional as the script ends anyway)
 cv2.destroyAllWindows()
