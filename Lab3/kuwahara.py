9f63beb674af857c653da9936ec6dfab22c982d3
Andrzej Rafalski <anraf1001@gmail.com>
2021-10-27 17:16:33 +0200

    Refactoring

--- Lab3/kuwahara.py
+++ Lab3/kuwahara.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def kuwahara(img, window_size=3):
+def kuwahara(img: np.ndarray, window_size=3):
     border_size = window_size - 1
     img_border_shape = (img.shape[0] + 2 * border_size,
                         img.shape[1] + 2 * border_size)
@@ -44,25 +44,26 @@ def kuwahara(img, window_size=3):
 
             img_new[row, col] = mean[np.argmax(std)]
 
-    return img_border  # TODO: Change to img_new
+    return img_new
 
 
-img = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
+if __name__ == '__main__':
+    img = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
 
-img_blurred = kuwahara(img)
+    img_blurred = kuwahara(img)
 
-cv2.namedWindow('image ori')
-cv2.namedWindow('image kuwahara')
+    cv2.namedWindow('image ori')
+    cv2.namedWindow('image kuwahara')
 
-while True:
-    # sleep for 10 ms waiting for user to press some key, return -1 on timeout
-    key_code = cv2.waitKey(10)
-    if key_code == 27:
-        # escape key pressed
-        break
+    while True:
+        # sleep for 10 ms waiting for user to press some key, return -1 on timeout
+        key_code = cv2.waitKey(10)
+        if key_code == 27:
+            # escape key pressed
+            break
 
-    cv2.imshow('image ori', img)
-    cv2.imshow('image kuwahara', img_blurred)
+        cv2.imshow('image ori', img)
+        cv2.imshow('image kuwahara', img_blurred)
 
-# closes all windows (usually optional as the script ends anyway)
-cv2.destroyAllWindows()
+    # closes all windows (usually optional as the script ends anyway)
+    cv2.destroyAllWindows()
