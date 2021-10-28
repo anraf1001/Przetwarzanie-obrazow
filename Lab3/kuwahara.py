29d063763e5a672ce74096e1ee2500a408ebb36a
Andrzej Rafalski <anraf1001@gmail.com>
2021-10-28 12:53:19 +0200

    Lab3 - kuwahara lab version

--- Lab3/kuwahara.py
+++ Lab3/kuwahara.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+from timeit import timeit
 
 
-def create_img_with_padding(img: np.ndarray, border_size):
+def create_img_with_padding(img: np.ndarray, border_size: int):
     img_border_shape = (img.shape[0] + 2 * border_size,
                         img.shape[1] + 2 * border_size)
 
@@ -29,37 +30,68 @@ def create_img_with_padding(img: np.ndarray, border_size):
     return img_border
 
 
-def kuwahara(img: np.ndarray, window_size=3):
+def kuwahara(img: np.ndarray, window_size: int = 3):
     border_size = window_size - 1
     img_border = create_img_with_padding(img, border_size)
     img_new = img.copy()
 
-    for row in range(len(img_new)):
-        for col in range(len(img_new[row])):
+    for row in range(img_new.shape[0]):
+        for col in range(img_new.shape[1]):
             mean = np.empty(4)
             std = np.empty(4)
 
-            mean[0], std[0] = cv2.meanStdDev(img_border[row: row + border_size + 1,
-                                             col: col + border_size + 1])
-            mean[1], std[1] = cv2.meanStdDev(img_border[row: row + border_size + 1,
-                                             col + border_size: col + 2 * border_size + 1])
-            mean[2], std[2] = cv2.meanStdDev(img_border[row + border_size: row + 2 * border_size + 1,
-                                             col: col + border_size + 1])
-            mean[3], std[3] = cv2.meanStdDev(img_border[row + border_size: row + 2 * border_size + 1,
-                                             col + border_size: col + 2 * border_size + 1])
+            mean[0], std[0] = cv2.meanStdDev(img_border[row: row + window_size,
+                                             col: col + window_size])
+            mean[1], std[1] = cv2.meanStdDev(img_border[row: row + window_size,
+                                             col + border_size: col + 2 * window_size])
+            mean[2], std[2] = cv2.meanStdDev(img_border[row + border_size: row + 2 * window_size,
+                                             col: col + window_size])
+            mean[3], std[3] = cv2.meanStdDev(img_border[row + border_size: row + 2 * window_size,
+                                             col + border_size: col + 2 * window_size])
 
             img_new[row, col] = mean[np.argmin(std)]
 
     return img_new
 
 
-if __name__ == '__main__':
+# From lab
+def apply_kuwahara(image: np.ndarray, window_size: int):
+    image_new = np.zeros_like(image)
+
+    for y in range(image_new.shape[0] - window_size):
+        for x in range(image_new.shape[1] - window_size):
+            window = image[y: y + window_size, x: x + window_size]
+            regions = [
+                window[0: window_size // 2 + 1, 0: window_size // 2 + 1],
+                window[window_size // 2: window_size, 0: window_size // 2 + 1],
+                window[0: window_size // 2 + 1, window_size // 2: window_size],
+                window[window_size // 2: window_size,
+                       window_size // 2: window_size]
+            ]
+
+            mean_std = [cv2.meanStdDev(r) for r in regions]
+            image_new[y + window_size // 2,
+                      x + window_size // 2] = min(mean_std, key=lambda x: x[1])[0]
+
+    return image_new
+
+
+def benchmark():
+    img = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
+    t1 = timeit(lambda: kuwahara(img), number=1)
+    t2 = timeit(lambda: apply_kuwahara(img, 5), number=1)
+    print(f'{t2 / t1 * 100:.2f}%')
+
+
+def main():
     img = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
 
     img_blurred = kuwahara(img)
+    img_blurred_2 = apply_kuwahara(img, 5)
 
     cv2.namedWindow('image ori')
     cv2.namedWindow('image kuwahara')
+    cv2.namedWindow('image kuwahara 2')
 
     while True:
         # sleep for 10 ms waiting for user to press some key, return -1 on timeout
@@ -70,6 +102,12 @@ if __name__ == '__main__':
 
         cv2.imshow('image ori', img)
         cv2.imshow('image kuwahara', img_blurred)
+        cv2.imshow('image kuwahara 2', img_blurred_2)
 
     # closes all windows (usually optional as the script ends anyway)
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # main()
+    benchmark()
