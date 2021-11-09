a840b1cd3a763816d87969f1e0478a46257700a1
Andrzej Rafalski <anraf1001@gmail.com>
2021-11-09 15:40:36 +0100

    Integral image

---
+++ Lab4/integral_image.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def integral_image(image: np.ndarray) -> np.ndarray:
+    integral = image.copy()
+
+    for i in range(1, integral.shape[1]):
+        integral[0, i] = integral[0, i] + integral[0, i - 1]
+
+    for i in range(1, integral.shape[0]):
+        integral[i, 0] = integral[i, 0] + integral[i - 1, 0]
+
+    for y in range(1, integral.shape[0]):
+        for x in range(1, integral.shape[1]):
+            integral[y, x] = integral[y, x] + integral[y - 1, x] + \
+                integral[y, x - 1] - integral[y - 1, x - 1]
+
+    return integral
+
+
+arr = np.array([[4, 5, 2, 1],
+                [0, 9, 3, 2],
+                [5, 6, 8, 1],
+                [2, 3, 0, 0]], dtype=np.uint8)
+gallery = cv2.imread('gallery.png')
+
+arr_integral = integral_image(arr)
+gallery_integral = integral_image(gallery)
+
+print(arr)
+print(arr_integral)
+# cv2.imshow('gallery', gallery)
+# cv2.waitKey()
