d3c0013d67b6e2a783d0766d90c81e3fcef17fe7
Andrzej Rafalski <anraf1001@gmail.com>
2021-11-09 13:45:46 +0100

    Lab4 - pug masking

--- Lab4/pug.py
+++ Lab4/pug.py
@@ -10,7 +10,10 @@ destination_points = np.float32([[598, 360], [681, 350], [683, 453], [598, 442]]
 matrix = cv2.getPerspectiveTransform(source_points, destination_points)
 pug_result = cv2.warpPerspective(pug, matrix, (gallery.shape[1], gallery.shape[0]))
 
-gallery[pug_result != 0] = 0
+_, pug_th = cv2.threshold(pug_result, 0, 255, cv2.THRESH_BINARY)
+pug_closed = cv2.morphologyEx(pug_th, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+gallery[pug_closed != 0] = 0
 gallery += pug_result
 
 cv2.imshow('gallery', gallery)
