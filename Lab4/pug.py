1f0c1d5da04072e04965b498054f1bf378434d75
Andrzej Rafalski <anraf1001@gmail.com>
2021-11-09 13:46:41 +0100

    Refactoring

--- Lab4/pug.py
+++ Lab4/pug.py
@@ -4,14 +4,23 @@ import numpy as np
 gallery = cv2.imread('gallery.png')
 pug = cv2.imread('pug.png')
 
-source_points = np.float32([[0, 0], [pug.shape[1], 0], [pug.shape[1], pug.shape[0]], [0, pug.shape[0]]])
-destination_points = np.float32([[598, 360], [681, 350], [683, 453], [598, 442]])
+source_points = np.float32([[0, 0],
+                            [pug.shape[1], 0],
+                            [pug.shape[1], pug.shape[0]],
+                            [0, pug.shape[0]]])
+destination_points = np.float32([[598, 360],
+                                 [681, 350],
+                                 [683, 453],
+                                 [598, 442]])
 
 matrix = cv2.getPerspectiveTransform(source_points, destination_points)
-pug_result = cv2.warpPerspective(pug, matrix, (gallery.shape[1], gallery.shape[0]))
+pug_result = cv2.warpPerspective(
+    pug, matrix, (gallery.shape[1], gallery.shape[0]))
 
 _, pug_th = cv2.threshold(pug_result, 0, 255, cv2.THRESH_BINARY)
-pug_closed = cv2.morphologyEx(pug_th, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+pug_closed = cv2.morphologyEx(pug_th,
+                              cv2.MORPH_CLOSE,
+                              np.ones((5, 5), np.uint8))
 
 gallery[pug_closed != 0] = 0
 gallery += pug_result
